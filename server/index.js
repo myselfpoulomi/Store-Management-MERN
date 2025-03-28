4dc46375b11d4d42574e5f0a31f1d80f2e538110
myselfpoulomi <routhpoulomi@gmail.com>
2025-03-28 23:34:34 +0530

    second comit

--- server/index.js
+++ server/index.js
@@ -6,6 +6,9 @@ app.get("/",(req,res)=>{
     res.send("api called")
 })
 
+console.log(process.env.MONGO_URL);
+
+
 app.listen (3000, ()=>{
     console.log("Server Started at http://localhost:3000");
     
