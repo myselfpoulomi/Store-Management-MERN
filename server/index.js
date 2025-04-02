65c2d64ecc418488a19b770c6dc7d8c84fcb9c3f
myselfpoulomi <routhpoulomi@gmail.com>
2025-04-02 22:29:31 +0530

    changes

--- server/index.js
+++ server/index.js
@@ -1,9 +1,20 @@
 import express from 'express'
+import dotenv from 'dotenv'
+
+dotenv.config();
 
 const app= express();
 
 app.get("/",(req,res)=>{
     res.send("api called")
+})
+
+app.post("/post",(req,res)=>{
+    res.send("post api called")
+})
+
+app.put("/put",(req,res)=>{
+    res.send("put api called")
 })
 
 console.log(process.env.MONGO_URL);
